e9f0112a3f60966161d28a9184940e9e6deb1567
Joao Delgado <joao.delgado@gmail.com>
2016-04-15 13:41:04 +0100

    Add budget controller

---
+++ DachsCashAPI/DachsCashAPI/Controllers/BudgetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using DachsCashAPI.Models;
+
+namespace DachsCashAPI.Controllers
+{
+    public class BudgetController : ApiController
+    {
+        public IEnumerable<BudgetModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BudgetModel Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DachsCashAPI/DachsCashAPI/Models/BudgetModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DachsCashAPI.Models
+{
+    public class BudgetModel
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
